a10396dd057d0bbce8c3531e91e08a5b1bbf658e
cyf <444978264@qq.com>
2021-02-25 17:45:20 +0800

    添加 快照

--- core/Event.ts
+++ core/Event.ts
@@ -1,4 +1,4 @@
-import { Emitter, EmitterEvent } from './Emitter'
+import { Disposable, Emitter, EmitterEvent } from './Emitter'
 
 interface NodeEmitter {
   on(event: string | symbol, listener: Function): unknown
@@ -22,5 +22,34 @@ export class Event {
       emitter.removeListener(eventName, fn)
     })
     return eventEmitter.event
+  }
+
+  static filter<T>(
+    event: EmitterEvent<T>,
+    filter: (...args: any[]) => boolean
+  ): EmitterEvent<T> {
+    return Event.snapshot(function (listener, ctx, disposables) {
+      return event(
+        function listenerWrap(...params) {
+          if (filter(...params)) {
+            listener.apply(ctx, params)
+          }
+        },
+        ctx,
+        disposables
+      )
+    })
+  }
+  // 快照
+  static snapshot<T>(event: EmitterEvent<T>) {
+    let listener: Disposable
+    const emitter = new Emitter<T>()
+    emitter.onDidFirstListen(function () {
+      listener = event(emitter.fire, emitter)
+    })
+    emitter.onDidLastRemove(function () {
+      listener.dispose()
+    })
+    return emitter.event
   }
 }
